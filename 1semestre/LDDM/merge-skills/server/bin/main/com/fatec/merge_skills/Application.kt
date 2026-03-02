a1c21f82ab6a325cb774b8188fdf3ec50bf97721
gialbanes <gialbanesantos@gmail.com>
2026-03-02 19:23:22 -0300

    feat: implementa database inicial

---
+++ 1semestre/LDDM/merge-skills/server/bin/main/com/fatec/merge_skills/Application.kt
@@ -0,0 +1,28 @@
+package com.fatec.merge_skills
+
+import com.fatec.merge_skills.db.DatabaseFactory
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+    DatabaseFactory.init() // Delegação do banco inserida na inicialização do Ktor
+
+    routing {
+        get("/") { call.respondText("Serviço Ktor ativo.") }
+        get("/health") { call.respondText("OK") }
+    }
+}

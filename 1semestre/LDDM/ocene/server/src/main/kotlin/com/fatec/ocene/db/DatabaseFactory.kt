185d6a14f5b850b7353fcd8469a222544c58314e
gialbanes <gialbanesantos@gmail.com>
2026-04-06 18:50:50 -0300

    fix: corrigido caminho de migração

--- 1semestre/LDDM/ocene/server/src/main/kotlin/com/fatec/ocene/db/DatabaseFactory.kt
+++ 1semestre/LDDM/ocene/server/src/main/kotlin/com/fatec/ocene/db/DatabaseFactory.kt
@@ -5,9 +5,8 @@ import org.jetbrains.exposed.sql.Database
 
 object DatabaseFactory {
     fun init() {
-        // Se não houver variável de ambiente, usa os valores EXATOS do seu docker-compose
         val dbUrl = System.getenv("DB_URL") ?: "jdbc:postgresql://localhost:5432/oceane"
-        val dbUser = System.getenv("DB_USER") ?: "devuser" // Corrigido de devpassword para devuser
+        val dbUser = System.getenv("DB_USER") ?: "devuser"
         val dbPassword = System.getenv("DB_PASSWORD") ?: "devpassword"
 
         println("Conectando ao banco: $dbUrl com o usuário: $dbUser")
@@ -15,7 +14,7 @@ object DatabaseFactory {
         try {
             val flyway = Flyway.configure()
                 .dataSource(dbUrl, dbUser, dbPassword)
-                .locations("classpath:com/fatec/oceane/db/migration")
+                .locations("classpath:com/fatec/ocene/db/migration")
                 .baselineOnMigrate(true)
                 .load()
 

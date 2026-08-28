7d3894ded5834214f4629c3a38cc3b3f592d00c2
Giovana Albanês <albanesgiovana@gmail.com>
2026-08-27 21:36:14 -0300

    config

---
+++ 2semestre/PLN/tokenizacao/exemplo01.py
@@ -0,0 +1,5 @@
+import nltk
+
+# quando instalar o nltk, é necessário baixar os pacotes de dados, pois por padrão só vem a lib em si
+# depois de executado uma vez, não precisa fazer mais em outros projetos 
+nltk.download('all')
